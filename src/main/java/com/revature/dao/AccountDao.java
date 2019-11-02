12b9fba6c6d1a1bfa00807bc7e07157f9dba0847
Jane <thejaneshin@gmail.com>
2019-11-02 15:28:08 -0400

    added start of account dao

--- src/main/java/com/revature/dao/AccountDao.java
+++ src/main/java/com/revature/dao/AccountDao.java
@@ -1,4 +1,18 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import com.revature.pojo.Account;
+
 public interface AccountDao {
+	public boolean createAccount(Account account);
+	
+	public Account readAccount(Account account);
+	
+	public boolean updateAccount(Account account);
+	
+	public boolean deleteAccount(Account account);
+	
+	public List<Account> readAllAccounts();
+	
 }
---
+++ src/main/java/com/revature/dao/AccountFakeDAOImpl.java
@@ -0,0 +1,41 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.pojo.Account;
+
+public class AccountFakeDAOImpl implements AccountDao {
+	
+	
+	
+	@Override
+	public boolean createAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Account readAccount(Account account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteAccount(Account account) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Account> readAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
