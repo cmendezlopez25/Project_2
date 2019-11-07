309266d09b780897cf4568a54fcc785ece87f16c
Jacob <pisecojacob@gmail.com>
2019-11-07 09:40:47 -0500

    Fixed the missing controller annotations

--- src/test/java/com/revature/test/dao/TransactionDaoImplTest.java
+++ src/test/java/com/revature/test/dao/TransactionDaoImplTest.java
@@ -1,14 +1,34 @@
 package com.revature.test.dao;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
 
+import com.revature.dao.TransactionDaoImpl;
+import com.revature.pojo.Account;
+import com.revature.pojo.Category;
+import com.revature.pojo.Transaction;
+import com.revature.util.SessionFactoryUtil;
+
+@ContextConfiguration(classes=SessionFactoryUtil.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TransactionDaoImplTest {
+	private TransactionDaoImpl transactionDao;
+	private Transaction existingTrans;
+	private Transaction badFormatTrans;
+	private Transaction newTrans;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,6 +40,16 @@ public class TransactionDaoImplTest {
 
 	@Before
 	public void setUp() throws Exception {
+		transactionDao = new TransactionDaoImpl();
+		
+		existingTrans = new Transaction(1, new Account(), 1.0, LocalDate.now(),
+				"breakfast", "I had a yummy breakfast", "no", new HashSet<Category>());
+		
+		badFormatTrans = new Transaction(2, new Account(), 0, LocalDate.now(),
+				"", "", "no", new HashSet<Category>());
+		
+		newTrans = new Transaction(3, new Account(), 4.56, LocalDate.now(),
+				"new transaction", " no note", "no", new HashSet<Category>());
 	}
 
 	@After
@@ -27,33 +57,24 @@ public class TransactionDaoImplTest {
 	}
 
 	@Test
-	public void testCreateTransaction() {
-		fail("Not yet implemented");
+	public void testCreateExistingTransaction() {
+		assertFalse(transactionDao.createTransaction(existingTrans));
+		Mockito.verify(transactionDao.getCurrentSession()).save(existingTrans);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void createTransactionNull() {
+		transactionDao.createTransaction(null);
+	}
+	
+	@Test
+	public void createNewTransaction() {
+		assertTrue(transactionDao.createTransaction(newTrans));
 	}
 
 	@Test
-	public void testReadTransaction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdateTransaction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDeleteTransaction() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testReadAllTransactions() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testReadAllTransactionsByAccount() {
-		fail("Not yet implemented");
+	public void createTransactionIncorrectFormat() {
+		assertFalse(transactionDao.createTransaction(badFormatTrans));
 	}
 
 }
